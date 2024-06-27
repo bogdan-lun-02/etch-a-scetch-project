91906c788a739d9a3bf72f529cf01c435dc67ad1
bogdan-lun-02 <bogdanlun36@gmail.com>
2024-06-28 00:36:00 +0300

    Created 16 divs with class square

---
+++ script.js
@@ -0,0 +1,8 @@
+for (let i = 0; i < 16; i++) {
+  document.querySelector('#container').appendChild(document.createElement('div'))
+}
+
+const squares = document.querySelectorAll('#container > div');
+for (let square of squares) {
+  square.classList.add('square');
+}
