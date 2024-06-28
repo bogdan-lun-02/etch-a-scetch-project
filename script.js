06a6d043abd6590609c3dfdb4bd6980e81e1f738
bogdan-lun-02 <bogdanlun36@gmail.com>
2024-06-28 21:01:16 +0300

    Added function prompting user for a size of a grid

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < 4096; i++) {
   document.querySelector('#container').appendChild(document.createElement('div'))
 }
 
 const squares = document.querySelectorAll('#container > div');
 for (let square of squares) {
   square.classList.add('square');
+}
+
+
+squares.forEach((square) => {
+  square.addEventListener('mouseover', () => {
+    square.style.background = 'red';
+  })
+})
+
+const button = document.querySelector('button');
+button.addEventListener('click', getUserInput);
+
+function getUserInput() {
+  let userInput = prompt('Choose a number of squares per size');
+  if (userInput > 100) {
+    alert('Wrong value! 100 is max!')
+  }
+
+  else return userInput;
 }
